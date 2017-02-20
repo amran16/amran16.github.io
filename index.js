2fa9d6c3454202cb11ce660af1ec1dc5b0849dac
amran16 <maryamg99@gmail.com>
2017-02-19 22:52:28 -0800

    added more projects

--- index.js
+++ index.js
@@ -1,47 +1,88 @@
 var projects =[
 
   {
-	name: 'Random Quote Machine',
-	img: './images/quote.png',
-	url: 'http://codepen.io/maryam106/pen/zNdXrV'
+  	name: 'Random Quote Machine',
+  	img: './images/quote.png',
+  	url: 'http://codepen.io/maryam106/full/zNdXrV',
+    detail: 'jquery & ajax'
   },
 
   {
-	name: 'Local Weather App',
-	img: './images/weather.png',
-	url: 'http://codepen.io/maryam106/pen/RGXwex'
+  	name: 'Local Weather App',
+  	img: './images/weather.png',
+    url: 'https://amran16.github.io/weather_app',
+  	//url: 'http://codepen.io/maryam106/full/RGXwex'
+    detail: 'jquery & ajax'
   },
 
   {
-	name: 'Wikipedia Viewer',
-	img: './images/wiki.png',
-	url: 'http://codepen.io/maryam106/pen/OWQvwG'
-  //url: 'http://codepen.io/maryam106/full/OWQvwG/' //doesn't work with full page
+  	name: 'Wikipedia Viewer',
+  	img: './images/wiki.png',
+  	url: 'http://codepen.io/maryam106/full/OWQvwG',
+    detail: 'jquery & ajax'
   },
 
   {
-	name: 'JavaScript Calculator',
-	img: './images/calculator.png',
-  url: 'http://codepen.io/maryam106/pen/apZxyM'
-	//url: 'https://amran16.github.io/js_calculator'
-  }
+  	name: 'JavaScript Calculator',
+  	img: './images/calculator.png',
+    //url: 'http://codepen.io/maryam106/full/apZxyM'
+  	url: 'https://amran16.github.io/js_calculator',
+    detail: 'jQuery'
+  },
+  {
+  	name: 'Timestamp Microservice',
+  	img: './images/timestamp.png',
+    url: 'https://timestamp-microservice-fcc1.herokuapp.com',
+    detail: 'Node & Express'
+  },
+
+  {
+  	name: 'Header Parser Microservice',
+  	img: './images/header_parser.png',
+    url: 'https://request-header-parser-fcc2.herokuapp.com',
+    detail: 'Node & Express'
+  },
+
+  {
+  	name: 'URL Shortner Microservice',
+  	img: './images/urlShortner.png',
+    url: 'https://maryamnumbertwo.herokuapp.com',
+    detail: 'Node, Express & MongoDB'
+  },
+
+  {
+  	name: 'File Metadata Microservice',
+  	img: './images/metadata.png',
+    url: 'https://protected-escarpment-73474.herokuapp.com',
+    detail: 'Node & Express'
+  },
+
+  {
+  	name: 'Markdown Previwer',
+  	img: './images/markdown.png',
+    //url: 'https://amran16.github.io/markdown_previewer',
+    url: 'http://codepen.io/maryam106/full/xgLbWE',
+    detail: 'React'
+  },
 
 ]
-
 
 projects.map(function(item){
 
   var title = item.name,
       image = item.img,
-    linkUrl = item.url;
+    linkUrl = item.url,
+    info = item.detail;
 
-	$('.cards').append(`
-		<a class="ui fluid card" target="_blank" href=${linkUrl}>
-			<div class="image">
-			  <img src=${image} alt=${name}>
-			</div>
-			  <div class="ui center aligned content">${title}</div>
-		</a>
+	$('.flex-container').append(`
+			<div class='flex-item'>
+       <a target='_blank' href=${linkUrl}>
+        <img src=${image} alt=${title} class='image'>
+         <h3 class='title'>${title}</h3>
+         <h7>${info}</h7>
+        </a>
+			 </div>
 		`)
 
+    //$('.flex-item').css('background-image', `img(${image})`);
 });
