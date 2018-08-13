6e8d17c9529c22f9c87894f5fa29f45491178fff
amran16 <maryamg99@gmail.com>
2018-08-13 10:58:17 -0700

    added more projects

--- src/Assets/projectsInfo.js
+++ src/Assets/projectsInfo.js
@@ -1,10 +1,21 @@
 
 const projects = [
+  {
+    name: 'Vote App',
+    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1534113643/vote.png',
+    url: 'https://wannavoteapp.herokuapp.com/',
+    detail: 'Node, Express & MongoDB'
+  },
+  {
+    name: 'Stock Market',
+    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1534111749/stock.png',
+    url: 'https://stocks-market-chart.herokuapp.com/',
+    detail: 'Node, Express & MongoDB'
+  },
 
   {
     name: 'NightLife',
-    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1533969034/nightlife.png',
-    //image: './images/nightlife.png',
+    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1534124650/nightlife.png',
     url: 'https://nightlifeproject.herokuapp.com/bars',
     detail: 'Node, Express & MongoDB'
   },
@@ -16,13 +27,13 @@ const projects = [
   },
   {
     name: 'Wikipedia Viewer',
-    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1533969032/wiki.png',
+    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1534111747/wiki.png',
     url: 'https://amran16.github.io/wikipedia_viewer',
     detail: 'jquery & ajax'
   },
   {
     name: 'Random Quote Machine',
-    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1533969035/quote.png',
+    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1534111747/quote.png',
     url: 'https://amran16.github.io/random_quote',
     detail: 'jquery & ajax'
   },
@@ -34,7 +45,7 @@ const projects = [
   },
   {
     name: 'Pomodor Clock',
-    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1533969033/pomodor.png',
+    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1534111746/pomodor.png',
     url: 'https://amran16.github.io/pomodoro',
     detail: 'JavaScript'
   },
@@ -46,7 +57,7 @@ const projects = [
   },
   {
     name: 'File Metadata Microservice',
-    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1533969034/metadata.png',
+    image: 'https://res.cloudinary.com/dv369opyl/image/upload/c_scale,h_320,w_600/v1534111747/metadata.png',
     url: 'https://protected-escarpment-73474.herokuapp.com',
     detail: 'Node & Express'
   }
