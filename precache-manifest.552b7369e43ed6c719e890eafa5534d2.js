833cc196fd3dc2b75b6224fc5790cb173b7fd6e3
amran16 <maryamg99@gmail.com>
2018-12-06 13:57:26 -0800

    Updates

---
+++ precache-manifest.552b7369e43ed6c719e890eafa5534d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4fb10c64fcea84983e27",
+    "url": "/static/js/main.4fb10c64.chunk.js"
+  },
+  {
+    "revision": "65e376d6077f7e4c621c",
+    "url": "/static/js/1.65e376d6.chunk.js"
+  },
+  {
+    "revision": "4fb10c64fcea84983e27",
+    "url": "/static/css/main.75267c84.chunk.css"
+  },
+  {
+    "revision": "70cc3efb84461ec8f6b9b01fc88d5ac8",
+    "url": "/index.html"
+  }
+];
